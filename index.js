4f47f8ae2377d74e42b42997cec7b1a2b9eb7311
Ravi Aswanth <r.raviaswanth@gmail.com>
2021-08-11 17:29:50 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,66 @@
+const {MongoClient} = require('mongodb');
 const express = require('express');
+const mongoose =require('mongoose');
 
-const app = express();
 
-const PORT = process.env.PORT || 5000;
+// const url = "mongodb+srv://raviaswanth:20440149@cluster0.6dqwv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(url, { useUnifiedTopology: true }); 
+// client.connect().then(client =>client.db().admin().listDatabases())
+//   .then(dbs => {
+//     console.log("Mongo databases", dbs);
+//   })
+//   .finally(() => client.close()); 
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+
+async function main(){
+
+    const uri = 'mongodb+srv://raviaswanth:20440149@cluster0.6dqwv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const client =new MongoClient(uri);
+
+try{
+    await client.connect();
+   console.log("connected");
+    await checkUsernameandPassword(client, {
+        loginUserName : 'arroyocolton@gmail.com',
+        loginPasswordName : 'arroyocolton',
+    });
+
+}catch(e){
+    console.log("error   "+e);
+}finally{
+    console.log("Done Connecting");
+    await client.close();
+}
+}
+ main().catch("err!!!!  "+console.error);
+
+async function listDatabase(client){
+const databasesList = await client.db().admin().listDatabases();
+console.log("Database :");
+databasesList.databases.forEach(db => {
+    console.log("Printing ____"+`-${db.name}`);
+
+})
+
+}
+
+
+async function checkUsernameandPassword(client,{
+    loginUserName= "",
+    loginPasswordName= "",
+}){
+    
+    const result =await client.db("HyruYou").collection("Talents").findOne({email:loginUserName});
+    
+    if (result){
+        console.log(`Found listing : '${loginUserName}'`);
+        console.log(result);
+     } else{
+        console.log("No Listing");
+     }
+    
+   
+    
+    }
+    
