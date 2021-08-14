b8670ef5368acc2ebd08fa00699ab3b2ca8711f3
Ravi Aswanth <r.raviaswanth@gmail.com>
2021-08-14 11:37:59 +1000

    New MongoDb

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({size: "30mb" , extended : true}));
 app.use(bodyParser.urlencoded({size: "30mb", extended : true}));
 app.use(cors());
 
-const CONNECTION_URL = "mongodb+srv://raviaswanth:20440149@cluster0.6dqwv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = "mongodb+srv://HyreYou:HyreYou@cluster0.qm47w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
